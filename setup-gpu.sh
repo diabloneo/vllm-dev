fa956576463a7f7e7c7ae37d1685c7e1a4db4baf
diabloneo <diabloneo@gmail.com>
2025-05-24 11:44:07 +0800

    Add setup-gpu.sh script
    Signed-off-by: diabloneo <diabloneo@gmail.com>

---
+++ setup-gpu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+# This projects setup a ubuntu 22.04 instance with a vLLM development environment.
+if [[ "$USER" -ne "ubuntu" ]]; then
+	echo "Please run as ubuntu"
+	exit 1
+fi
+
+echo "Changing to home directory"
+cd ~
+
+echo "Installing development dependencies"
+sudo apt-get update -y
+sudo apt-get install -y gcc-12 g++-12 libnuma-dev python3-dev
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 10 --slave /usr/bin/g++ g++ /usr/bin/g++-12
+
+echo "Installing uv"
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv version
+
+echo "Create venv for vllm"
+uv venv venv --python 3.12 --seed
+
+echo "Cloning vllm project"
+git clone https://github.com/vllm-project/vllm.git
